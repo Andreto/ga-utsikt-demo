b55b0b8db38edb06951c19c83d1bb3831a94213a
Andreas Törnkvist <andreas.o.tornkvist@gmail.com>
2022-02-07 15:28:23 +0100

    demo, init

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var locatorButton = document.getElementById('locator-button');
 var locatorSvg = document.getElementById('locator-svg');
 var sateliteButton = document.getElementById('satelite-button');
 var sateliteSvg = document.getElementById('satelite-svg');
+var nextDemoElem = document.getElementById('demo-right');
+var prevDemoElem = document.getElementById('demo-left');
 
 var sateliteMapOn = false;
 var blockMapClick = false;
@@ -44,15 +46,27 @@ var calcLocation = L.marker([59.33, 18.07], {
 var pl, hz; // Map-items that displays view-poly-lines and horizon-line
 
 ////// MAP-DEMO //////
+var pl;
 
 calcDemos = [
-    {'obj': cdNrk, 'name': 'Svältmon, Norrköping', 'lat': 59.58, 'lon': 18.02},
-    {'obj': cdKeb, 'name': 'Kebnekaises nordtopp', 'lat': 59.58, 'lon': 18.02},
+    {'obj': cdNrk, 'name': 'Svältmon, Norrköping', 'lat': 58.654138, 'lon': 16.077654, 'vh': 2},
+    {'obj': cdKeb, 'name': 'Kebnekaises nordtopp', 'lat': 67.903667, 'lon': 18.526145, 'vh': 2},
+    {'obj': cdAre, 'name': 'Härjångsfjällen, Åre', 'lat': 63.019267, 'lon': 12.590838, 'vh': 2},
+    {'obj': cdJan, 'name': 'Långe Jan, Ölands södra uddde', 'lat': 56.196162, 'lon': 16.398396, 'vh': 39},
 ]
 
-calcLocation.setLatLng();
+function setDemo() {
+    calcLocation.setLatLng([calcDemos[demoI].lat, calcDemos[demoI].lon]);
+    pl = L.polyline(calcDemos[demoI].obj, { color: '#B13A3C', weight: 2 }).addTo(map);
+    map.flyTo([calcDemos[demoI].lat, calcDemos[demoI].lon], 8);
+    document.getElementById('obshInput').value = calcDemos[demoI].vh;
+    document.getElementById('demo-name').innerText = calcDemos[demoI].name;
+    document.getElementById('lat-disp').innerText = calcDemos[demoI].lat;
+    document.getElementById('lon-disp').innerText = calcDemos[demoI].lon;
+}
 
-
+demoI = Math.floor(Math.random()*calcDemos.length);
+setDemo();
 
 
 function onResize(e) {
@@ -69,7 +83,7 @@ scaleElem.innerHTML = '<div class="scale-indicator-text" id="scale-ind">100 m</d
 document.getElementsByClassName('leaflet-control-container')[0].appendChild(scaleElem);
 
 // Define event-listeners
-map.on('click', onMapClick);
+map.on('click');
 map.on('zoomend', onResize);
 L.control.scale().addTo(map);
 
@@ -106,4 +120,16 @@ sateliteSvg.addEventListener('mouseover', function () {
 });
 sateliteSvg.addEventListener('mouseout', function () {
     blockMapClick = false;
+});
+
+nextDemoElem.addEventListener('click', function () {
+    map.removeLayer(pl);
+    demoI = (demoI+1)%calcDemos.length;
+    setDemo();
+});
+
+prevDemoElem.addEventListener('click', function () {
+    map.removeLayer(pl);
+    demoI = (demoI-1+calcDemos.length)%calcDemos.length;
+    setDemo();
 });
